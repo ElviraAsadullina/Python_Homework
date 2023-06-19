bd71852ba2265a715b572aa8bf1582d705b40225
Elvira <eliasufa@yahoo.com>
2023-06-19 17:34:51 +0500

    Updated task_2 and task_groupwork according to teacher's remarks

--- Homework_2/task_2.py
+++ Homework_2/task_2.py
@@ -10,8 +10,11 @@ while True:
 
 def reduce_fraction(x, y):
     m = min(x, y)
-    if x % y == 0 or y % x == 0:
-        x, y = x / m, y / m
+    if m == 0:
+        return 0
+    else:
+        if x % y == 0 or y % x == 0:
+            x, y = x / m, y / m
     return f'{int(x)}/{int(y)}'
 
 
