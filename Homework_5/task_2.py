b55fe455c3d59b01effd1d8a0a6a23b28a6aeb30
Elvira <eliasufa@yahoo.com>
2023-06-29 16:25:42 +0500

    Homework 5 task_2 updated

--- Homework_5/task_2.py
+++ Homework_5/task_2.py
@@ -2,12 +2,10 @@
 # имена str, ставка int, премия str с указанием процентов вида «10.25%». В результате получаем
 # словарь с именем в качестве ключа и суммой премии в качестве значения. Сумма рассчитывается как
 # ставка умноженная на процент премии.
-def get_dict(lst_1, lst_2, lst_3):
-    return ({n: s / 100 * float(b[:-1])} for n, s, b in zip(lst_1, lst_2, lst_3))
-
-
 names = ['Ruslan', 'Pavel', 'Dmitry']
 rate = [100_000, 110_000, 120_000]
 bonus = ['10.25%', '12.5%', '20.0%']
 
-print(*get_dict(names, rate, bonus))
+table_gen = ({n: s / 100 * float(b[:-1])} for n, s, b in zip(names, rate, bonus))
+
+print(*table_gen)
