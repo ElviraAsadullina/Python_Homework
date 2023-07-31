50689bcd494185b23655ec653c196b1bddfa6418
Elvira Asadullina <110663957+ElviraAsadullina@users.noreply.github.com>
2023-07-31 22:22:42 +0500

    Update test_pytest_Project.py

--- Homework_14/test_pytest_Project.py
+++ Homework_14/test_pytest_Project.py
@@ -1,3 +1,6 @@
+# На семинаре 13 был создан проект по работе с пользователями (имя, id, уровень).
+# Напишите 3-7 тестов pytest для данного проекта.
+# Используйте фикстуры.
 import json
 from tempfile import NamedTemporaryFile
 import pytest
